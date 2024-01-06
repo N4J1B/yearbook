7ec62c2c730657a3f05677f3c25d60d9f64047ba
N4J1B <48822699+N4J1B@users.noreply.github.com>
2024-01-06 08:59:18 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\MuridController;
-=======
->>>>>>> bbd35ca4a2bebd9084ef4a35e345c66d6159f7c3
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,8 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
 
 Route::apiResource('murid', MuridController::class);
-=======
->>>>>>> bbd35ca4a2bebd9084ef4a35e345c66d6159f7c3
