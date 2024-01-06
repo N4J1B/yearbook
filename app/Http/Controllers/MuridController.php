5ecf96920f81a3759c836973b8766e008d57449a
N4J1B <najibainun756@gmail.com>
2024-01-06 08:07:14 +0700

    "SECOND COMMIT"

---
+++ app/Http/Controllers/MuridController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\murid;
+use Illuminate\Http\Request;
+
+class MuridController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $murid = murid::paginate(5);
+        return $murid;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(murid $murid)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(murid $murid)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, murid $murid)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(murid $murid)
+    {
+        //
+    }
+}
